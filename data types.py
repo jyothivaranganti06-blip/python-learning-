557f2ec5ada3457cb2a14bb522ff2567bd4c7496
jyothi <jyothivaranganti06>
2026-05-29 21:09:22 +0530

    data types in pyhton

---
+++ data types.py
@@ -0,0 +1,24 @@
+
+#Python has different data types like text type :'string', Numeric types:'int,float,complex', Sequence types:'list,tuple...
+
+
+# the integer value has the 'int' data type
+a=5
+d=6
+c=a+d
+print(c)
+
+# the floating number has 'float' data type
+n=float(input("enter a number: ")) # taking the input from the user
+m=float(input("enter another number: "))
+print(n+m)
+
+#progam to check the type of a variable
+p="python"
+print(type(p))
+
+
+
+
+
+
