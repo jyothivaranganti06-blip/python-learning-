a7cb7a674b46bf9d72e7a32f47fcc4beb248dee1
jyothi <jyothivaranganti06>
2026-05-30 21:57:17 +0530

    learned about operators in pyhton

---
+++ operators1.py
@@ -0,0 +1,66 @@
+
+#python has different types of the operators like arithmetic, relational,logical,bitwise,assignment operators
+
+#arithmetic operator
+#addition
+n=34
+m=40
+print(n+m)
+
+#subtraction
+a=50
+b=20
+print(a-b)
+
+#multiplication
+s=3
+c=9
+print(s*c)
+
+#division
+e=40
+f=5
+print(e/f)
+
+#modules
+x=15
+y=4
+print(x%y) # it gives the reminder value after division
+
+#floor division
+s=40
+t=5
+print(s//t)# returns the integer value without having any decimal points
+
+#exponentiation
+p=3
+o=4
+print(p**o)# multiplies the number for the no.of times
+
+
+#relational operators 
+a=5
+b=2
+print(a>b) # '5' is greater then '2' so, it gives "TRUE"
+
+d=3
+f=3
+print(d>=f) #gives "TRUE"
+
+
+e=9
+f=8
+print(e<f)# gives"FALSE"
+
+
+r=80
+t=80
+print(r==t) # This operator compare the two values ; it gives "true"
+
+
+t=90
+r=40
+print(t==r)# gives "FALSE"
+
+
+
