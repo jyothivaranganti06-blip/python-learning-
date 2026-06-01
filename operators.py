a9f07b4a6c1b075dd253ef8d6a247ee09841424d
jyothi <jyothivaranganti06>
2026-06-01 12:51:24 +0530

    assignment operators in python operators

---
+++ operators.py
@@ -0,0 +1,31 @@
+
+#python has different types of the operators like arithmetic, relational,logical,bitwise,assignment operators
+#arithmetic operator
+a=45
+b=60
+print(a+b)
+
+#subtraction
+s=67
+t=34
+print(s-t)
+
+
+#multiplication
+e=6
+f=5
+print(e*f)
+
+
+#division
+r=56
+q=5
+print(r/q)
+
+#module
+p=22
+o=10
+print(p%o)
+
+
+
