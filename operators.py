183b66db3870fad735fa539e74884ede0f5e27bc
jyothi <jyothivaranganti06>
2026-06-06 19:14:17 +0530

    identity,membership,bitwise operators

--- operators.py
+++ operators.py
@@ -54,3 +54,73 @@ print(f<=g)
 f=87
 e=87
 print(f==e)
+
+#chaning comparision operator
+d=5
+print(1<d<10)
+print(1<d and d<10)
+
+#logical operators 
+#logical AND
+s=3
+g=7
+print(s>1 and s<10)
+
+#logical OR
+d=56
+h=34
+print(d<200 or d>50)
+
+#logical NOT
+g=34
+print(not g>50) 
+print(not g<12)
+print(not(g>32 and g<100))
+
+
+#assignment operators 
+a=45
+a+=4  #add and assign
+print(a)
+
+a=67
+a-=3 #subtract and assign
+print(a)
+
+f=4
+f*=2  #multiply and assign
+print(f)
+
+g=50
+g/=2 #divide and assign
+print(g)
+
+n=45
+n//=2 #floor division and assign
+print(n)
+
+b=67
+b**=2 #power and assign 
+print(b)
+
+g=34
+g%=10 #module and assign
+print(g)
+
+h=56
+h&=2 #logical AND and assign
+print(h)
+
+y=78
+y|=3 #logical OR and assign
+print(y)
+
+
+r=56
+r>>=2 #right shift and assign
+print(r)
+
+
+t=67
+t<<=2 #left shift and assign
+print(t)
