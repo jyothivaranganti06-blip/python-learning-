a9f0552870f60f8a04a03a403f62d790f0a5daa0
jyothi <jyothivaranganti06>
2026-07-12 14:57:11 +0530

    temperature converter in python

---
+++ temperature converter.py
@@ -0,0 +1,10 @@
+
+#temperature converter is a converter that convert the celsius to fahrenheit
+temp=int(input("enter the temperature in celsius: "))
+fehrenheit=(temp *9/5)+32
+print(fehrenheit)
+
+#convert the fehrenheit to celsius
+temp=int(input("enter the temperature in fehrenheit: "))
+celsius=(temp-32)*5/9
+print(celsius)
