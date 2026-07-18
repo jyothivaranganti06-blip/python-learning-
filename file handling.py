a27c3e37c672c6f8c37143274f304fea6fffdf5e
jyothi <jyothivaranganti06>
2026-07-18 20:28:41 +0530

    file handling (student.txt,students.csv,employees.csv)

---
+++ file handling.py
@@ -0,0 +1,189 @@
+#file=open("student.txt","x") #creates a  new file
+#file.close()
+
+file=open("student.txt","r") 
+file.close()
+
+file=open("student.txt","w") 
+print(file.write("hello there i'm a computer science student")) #writes in the created file
+file.close() #if we again wrote the "w" code it erases all the previous data 
+
+with open("student.txt")as file:
+    print(file.read(5)) #get the word by its length
+
+#reads the file 
+file=open("student.txt","r")
+print(file.read())
+file.close()
+
+file=open("student.txt","a")
+print(file.write("\npursuing graduation" )) #append mode adds a data without removing previous one
+file.close()
+
+#read+write
+file=open("student.txt","r+")
+print(file.read())
+file.write("\nsubjects\npython\nc\ndatabase")
+file.close()
+
+#write+read
+#file=open("student.txt","w+")
+#file.write("java") #removes all the previous data before entering new data
+#file.seek(0)
+#print(file.read())
+#file.close()
+
+#append+read
+file=open("student.txt","a+")
+file.write("\njava")
+file.seek(0)
+print(file.read())
+file.close()
+
+#reading files
+file=open("student.txt","r")
+print(file.read())
+file.close()
+
+#readline
+file=open("student.txt","r")
+print(file.readline())
+file.close()
+
+#readdlines
+file=open("student.txt","r")
+print(file.readlines())
+file.close()
+
+#writing files
+
+#writelines()
+#file=open("student.txt","w")
+#file.write("r language\n")
+#lines=["python\n","java\n","SQL\n"]
+#file.writelines(lines)
+#file.close()
+
+#append data
+file=open("student.txt","a")
+file.write("\nmachine learning")
+file.close()
+#closing files
+file.close()
+
+
+#using with
+#file=open("me.txt","x")     #creates a new file called me.txt
+#file.close()
+
+#with open("me.txt","r") as file:
+     #print(file.read())  #it doesn't has close python automatically closes it 
+#with open("me.txt","w") as file:
+     #print(file.write("hii"))
+#with open("me.txt","w") as file:
+     #lines=["i'm a student\n","iam focusing on pyhton\n","currently learning file handling\n"]
+     #print(file.writelines(lines))
+#with open("me.txt","a") as file:
+     #print(file.write("afterr that i will focus on DSA\n"))
+
+# file pointer
+
+#tell() tell returns the current position of the file pointer
+file=open("student.txt","r")
+print(file.tell()) 
+file.close()
+
+file=open("student.txt","r")
+print(file.read(12))
+print(file.tell())
+file.close()
+
+#seek  seek moves the file pointer to a specific position in the file
+file=open("student.txt","r")
+file.seek(0)
+print(file.read())
+file.close()
+
+file=open("student.txt","r")
+file.seek(12)
+print(file.read())
+file.close()
+
+#tell and seek
+file=open("student.txt","r")
+print(file.tell())
+file.seek(16)
+print(file.tell())
+print(file.read())
+file.close()
+file=open("student.txt","r")
+print(file.read())
+file.seek(0)
+print(file.read())
+file.close()
+#check if the file exists
+import os
+print(os.path.exists("student.txt")) #true
+
+#rename file
+#import os
+#os.rename("student1.txt","student.txt")
+
+#delete file
+#import os
+#os.remove("me.txt")
+#csv files  csv=comma seperated values
+import csv
+with open("students.csv","w",newline="") as  file:
+     writer=csv.writer (file)
+     writer.writerow(["Name","Age","Course"])
+     writer.writerow(["james","20","computer science"])
+     writer.writerow(["juhoon","18","physics"])
+     writer.writerow(["martin","18","chemistry"])
+
+#read a csv file
+with open("students.csv","r") as file:
+     reader=csv.reader(file)
+     for row in reader:
+          print(row)
+
+#write multiple rows
+import csv
+with open("students.csv","w",newline="")as file:
+     writer=csv.writer(file)
+     writer.writerows([["name","age","course"],
+                       ["pinky",20,"computer science"],
+                       ["james",20,"computer science"],
+                       ["rahul",22,"chemistry"]])
+#append a new row
+import csv
+with open("students.csv","a",newline="")as file:
+     writer=csv.writer(file) 
+     writer.writerow(["juhoon",20,"commputer science"])
+
+#read as dictionary
+import csv
+with open("students.csv","r")as file:
+     reader=csv.DictReader(file)
+     for row in reader:
+          print(row)
+#write using dictionary
+#import csv
+#with open("students.csv","w",newline="")as file:
+    # fields=["name","age","course"]
+    # writer=csv.DictWriter(file,fieldnames=fields)
+     #writer.writeheader()
+     #writer.writerow({
+       #   "name":"james",
+      #    "age":20,
+     #     "course":"computer science"
+    # })
+
+import csv
+with open("employees.csv","w",newline="")as file:
+     writer= csv.writer(file)
+     writer.writerows([["name","age","department"],
+     ["jennie",30,"IT"],
+     ["mina",26,"HR"],
+      ["jihyo",25,"IT"]])
+     
